395ab61c3e71dd45f43ab3049c15525e888c7f57
hecfb <148881381+hecfb@users.noreply.github.com>
2023-12-04 15:54:49 -0500

    Update Send Reminder.py

--- Send Reminder.py
+++ Send Reminder.py
@@ -5,35 +5,23 @@ import logging
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-sns = boto3.client('sns')
-ses = boto3.client('ses')
 
+sqs = boto3.client('sqs')
+queue_url = 'YOUR_SQS_QUEUE_URL'  
 
 def lambda_handler(event, context):
     for record in event['Records']:
         if record['eventName'] == 'REMOVE':
             reminder = record['dynamodb']['OldImage']
-            message = reminder['message']['S']
-            notification_type = reminder['notificationType']['S']
-
-            if notification_type == 'sms':
-                sns.publish(
-                    PhoneNumber=reminder['userId']['S'],
-                    Message=message
-                )
-            elif notification_type == 'email':
-                ses.send_email(
-                    Source='from@example.com',
-                    Destination={'ToAddresses': [reminder['userId']['S']]},
-                    Message={
-                        'Subject': {'Data': 'Reminder'},
-                        'Body': {'Text': {'Data': message}}
-                    }
-                )
-
-            logger.info(f"Sent reminder: {message}")
+            message_body = {
+                'userId': reminder['userId']['S'],
+                'message': reminder['message']['S'],
+                'notificationType': reminder['notificationType']['S']
+            }
+            sqs.send_message(QueueUrl=queue_url, MessageBody=json.dumps(message_body))
+            logger.info(f"Sent message to SQS: {message_body}")
 
     return {
         'statusCode': 200,
-        'body': json.dumps('Reminders sent successfully')
+        'body': json.dumps('Messages sent to SQS successfully')
     }
